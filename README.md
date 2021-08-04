b389cd2c18d86ca62cdabdf0ca5662b345127b41
Rohan Poreddy <poreddyrohan@gmail.com>
2021-08-03 20:35:16 -0700

    changes made in readme, div, sub files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # test_git
 #Hello This repo tell us about practising git commits in the list
 #This is the new version.
+
+#A new branch 'new' is added.
--- div.cpp
+++ div.cpp
@@ -6,5 +6,7 @@ int main(){
 	int a = 10, b = 5, div;
 	div = a / b; 
 	cout << div << endl;
+	cout << div - a << endl;
+	cout << div + b << endl;
 	return 0;
 }
