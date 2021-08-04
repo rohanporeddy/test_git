3a2f9d32e5bc42fbb15875947fc5d8a9761a835b
rohanporeddy <31199275+rohanporeddy@users.noreply.github.com>
2021-08-03 20:23:53 -0700

    updated add file
    added some lines

--- add.cpp
+++ add.cpp
@@ -5,6 +5,9 @@ int main(){
 	int a = 10, b = 20, sum;
 	sum = a + b;
 	cout << sum << endl;
-	cout << sum + a << endl;	
+	cout << sum + a << endl;
+	
+	cout << sum + a + b << endl;
+	cout << a - b << endl;
 	return 0;
 }
